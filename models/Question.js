6cadd5516f447fed98acd3df10e8829322b2dd62
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-12-23 02:08:21 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -32,6 +32,7 @@ class Question{
   static Find(id){
     const sql = `SELECT * FROM questions WHERE id = ?`
     return new Promise(function(resolve){
+      db.get(sql, [id], function(err, resultRow){
       const question = new Question()
       resolve(question)
     })
