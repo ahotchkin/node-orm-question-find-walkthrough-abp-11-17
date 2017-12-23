296da41f42928950cde0d2562524fd6d58789561
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-12-23 02:05:15 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -31,7 +31,7 @@ class Question{
 
   static Find(id){
     return new Promise(function(resolve){
-      const question = new Question
+      const question = new Question()
       resolve(question)
     })
   }
